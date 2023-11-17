9f719fee6b2c08a474bc4bca8e63a7422ac70d08
chenhu.pei <pch1024@outlook.com>
2023-11-17 18:39:33 +0800

    .

--- docs/assets/javascript-3323d45a.js
+++ docs/assets/javascript-3323d45a.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-0d6b2c94.js";import"./index-64cefdb3.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-401bc42c.js";import"./index-f487c266.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.44.0(3e047efd345ff102c8c61b5398fb30845aaac166)
  * Released under the MIT license
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: '/vscode/',
+    base: "/vscode/",
     build: {
+        chunkSizeWarningLimit: 500, // KB
         outDir: "docs",
     },
 });
