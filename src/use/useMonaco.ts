401cc084c3f511012a08fafaa9642f38075e4b5f
chenhu.pei <pch1024@outlook.com>
2023-11-17 18:43:46 +0800

    .

--- src/use/useMonaco.ts
+++ src/use/useMonaco.ts
@@ -3,20 +3,21 @@
  * https://github.com/vitejs/vite/discussions/1791#discussioncomment-321046
  * https://twitter.com/youyuxi/status/1355316139144970240?s=20
  */
-import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
+import * as monaco from "monaco-editor";
+// import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
 // import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
 // import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
 // import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
 import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
-import "monaco-editor/esm/vs/basic-languages/typescript/typescript.contribution"; // 代码高亮&提示
-import "monaco-editor/esm/vs/basic-languages/javascript/javascript.contribution"; // 代码高亮&提示
-import "monaco-editor/esm/vs/language/typescript/monaco.contribution"; // 代码高亮&提示
-import "monaco-editor/esm/vs/editor/contrib/contextmenu/browser/contextmenu.js"; // 右键显示菜单
-import "monaco-editor/esm/vs/editor/contrib/folding/browser/folding.js"; // 折叠
-import "monaco-editor/esm/vs/editor/contrib/format/browser/formatActions.js"; // 格式化代码
-import "monaco-editor/esm/vs/editor/contrib/suggest/browser/suggestController.js"; // 代码联想提示
-import "monaco-editor/esm/vs/editor/contrib/tokenization/browser/tokenization.js"; // 代码联想提示
+// import "monaco-editor/esm/vs/basic-languages/typescript/typescript.contribution"; // 代码高亮&提示
+// import "monaco-editor/esm/vs/basic-languages/javascript/javascript.contribution"; // 代码高亮&提示
+// import "monaco-editor/esm/vs/language/typescript/monaco.contribution"; // 代码高亮&提示
+// import "monaco-editor/esm/vs/editor/contrib/contextmenu/browser/contextmenu.js"; // 右键显示菜单
+// import "monaco-editor/esm/vs/editor/contrib/folding/browser/folding.js"; // 折叠
+// import "monaco-editor/esm/vs/editor/contrib/format/browser/formatActions.js"; // 格式化代码
+// import "monaco-editor/esm/vs/editor/contrib/suggest/browser/suggestController.js"; // 代码联想提示
+// import "monaco-editor/esm/vs/editor/contrib/tokenization/browser/tokenization.js"; // 代码联想提示
 
 self.MonacoEnvironment = {
     getWorker(_, label) {
